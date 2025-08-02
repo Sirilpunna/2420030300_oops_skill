3f20e20f660baa612304462775526a2ea645e6d7
Sirilpunna <siril.punna@gmail.com>
2025-08-02 09:10:25 +0530

    Wrapper class

---
+++ wrapper.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class wrapper {
+public static void main(String args[]) {
+	Integer a=564;
+	Boolean b=true;
+	Double d=9.87d;
+	int c=10;
+	System.out.println(a.toString().length());
+	System.out.println(d.toString().length());
+}
+}
